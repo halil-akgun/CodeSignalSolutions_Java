d387fad21f31c6f3b34ba286b186a998040478e2
halil <halilakgun0@gmail.com>
2023-02-10 20:39:14 +0300

    CS25_ArrayReplace

---
+++ src/CS25_ArrayReplace.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+/*
+Given an array of integers, replace all the occurrences of elemToReplace with substitutionElem.
+
+Example
+For inputArray = [1, 2, 1], elemToReplace = 1, and substitutionElem = 3, the output should be
+solution(inputArray, elemToReplace, substitutionElem) = [3, 2, 3].
+ */
+public class CS25_ArrayReplace {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(solution(new int[]{1, 2, 1}, 1, 3)));
+    }
+
+    static int[] solution(int[] inputArray, int elemToReplace, int substitutionElem) {
+        int[] result = new int[inputArray.length];
+        for (int i = 0; i < inputArray.length; i++) {
+            if (inputArray[i] == elemToReplace) result[i] = substitutionElem;
+            else result[i] = inputArray[i];
+        }
+        return result;
+    }
+}
