3f14351489fd7846823f821779721c09c725692a
halil <halilakgun0@gmail.com>
2023-04-08 21:15:24 +0300

    CS47_isDigit

---
+++ src/CS47_isDigit.java
@@ -0,0 +1,18 @@
+/*
+Determine if the given character is a digit or not.
+
+Example
+For symbol = '0', the output should be
+solution(symbol) = true;
+For symbol = '-', the output should be
+solution(symbol) = false.
+ */
+public class CS47_isDigit {
+    public static void main(String[] args) {
+        System.out.println(solution('a'));
+    }
+
+    static boolean solution(char symbol) {
+        return symbol >= '0' && symbol <= '9';
+    }
+}
