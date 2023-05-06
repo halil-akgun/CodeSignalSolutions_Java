abc5602dafe19fc07378516c164b969e03fc7791
halil <halilakgun0@gmail.com>
2023-05-07 00:21:33 +0300

    CS05_MaxMultiple

---
+++ src/arcade/thecore/CS05_MaxMultiple.java
@@ -0,0 +1,28 @@
+package arcade.thecore;
+
+/*
+Given a divisor and a bound, find the largest integer N such that:
+
+N is divisible by divisor.
+N is less than or equal to bound.
+N is greater than 0.
+It is guaranteed that such a number exists.
+
+Example
+For divisor = 3 and bound = 10, the output should be
+solution(divisor, bound) = 9.
+
+The largest integer divisible by 3 and not larger than 10 is 9.
+ */
+public class CS05_MaxMultiple {
+    public static void main(String[] args) {
+        System.out.println(solution(3, 10));
+    }
+
+    static int solution(int divisor, int bound) {
+        for (int i = bound; i > 0; i--) {
+            if (i % divisor == 0) return i;
+        }
+        return 0;
+    }
+}
