e970cba03d8d4381dadc60091741cb572f76e13b
halil <halilakgun0@gmail.com>
2023-05-12 17:52:55 +0300

    CS09_ReachNextLevel

---
+++ src/arcade/thecore/CS09_ReachNextLevel.java
@@ -0,0 +1,24 @@
+package arcade.thecore;
+
+/*
+You are playing an RPG game. Currently, your experience points (XP) total is equal to experience.
+To reach the next level, your XP should be at least at threshold. If you kill the monster in front of you,
+you will gain more experience points in the amount of the reward.
+
+Given values experience, threshold and reward, check if you reach the next level after killing the monster.
+
+Example
+For experience = 10, threshold = 15, and reward = 5, the output should be
+solution(experience, threshold, reward) = true;
+For experience = 10, threshold = 15, and reward = 4, the output should be
+solution(experience, threshold, reward) = false.
+ */
+public class CS09_ReachNextLevel {
+    public static void main(String[] args) {
+        System.out.println(solution(10, 15, 5));
+    }
+
+    static boolean solution(int experience, int threshold, int reward) {
+        return experience + reward >= threshold;
+    }
+}
