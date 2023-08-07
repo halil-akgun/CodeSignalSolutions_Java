ba8b0f70ded8929339ba4ec3d248537ee064be19
halil <halilakgun0@gmail.com>
2023-08-07 15:54:21 +0300

    CS046_PagesNumberingWithInk

---
+++ src/arcade/thecore/CS046_PagesNumberingWithInk.java
@@ -0,0 +1,41 @@
+package arcade.thecore;
+
+/*
+You work in a company that prints and publishes books. You are responsible for designing the page numbering
+mechanism in the printer. You know how many digits a printer can print with the leftover ink. Now you want
+to write a function to determine what the last page of the book is that you can number given the current page
+and numberOfDigits left. A page is considered numbered if it has the full number printed on it (e.g. if we are
+working with page 102 but have ink only for two digits then this page will not be considered numbered).
+
+It's guaranteed that you can number the current page, and that you can't number the last one in the book.
+
+Example
+For current = 1 and numberOfDigits = 5, the output should be
+solution(current, numberOfDigits) = 5.
+The following numbers will be printed: 1, 2, 3, 4, 5.
+
+For current = 21 and numberOfDigits = 5, the output should be
+solution(current, numberOfDigits) = 22.
+The following numbers will be printed: 21, 22.
+
+For current = 8 and numberOfDigits = 4, the output should be
+solution(current, numberOfDigits) = 10.
+The following numbers will be printed: 8, 9, 10.
+ */
+public class CS046_PagesNumberingWithInk {
+    public static void main(String[] args) {
+        System.out.println(solution(9, 4));
+    }
+
+    static int solution(int current, int numberOfDigits) {
+        int lastPage = current;
+        int length = String.valueOf(current).length();
+        if (numberOfDigits < length) return 0;
+        while (length <= numberOfDigits) {
+            lastPage++;
+            numberOfDigits -= length;
+            length = String.valueOf(lastPage).length();
+        }
+        return lastPage - 1;
+    }
+}
