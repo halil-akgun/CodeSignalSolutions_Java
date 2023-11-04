ab3297e0a49332d7b9244206da8caba5915ad2f3
halil-akgun <halilakgun0@gmail.com>
2023-11-04 10:28:16 +0300

    CS53_IsTandemRepeat

---
+++ src/arcade/thecore/CS53_IsTandemRepeat.java
@@ -0,0 +1,22 @@
+package arcade.thecore;
+
+/*
+Determine whether the given string can be obtained by one concatenation of some string to itself.
+
+Example
+For inputString = "tandemtandem", the output should be
+solution(inputString) = true;
+For inputString = "qqq", the output should be
+solution(inputString) = false;
+For inputString = "2w2ww", the output should be
+solution(inputString) = false.
+ */
+public class CS53_IsTandemRepeat {
+    public static void main(String[] args) {
+        System.out.println(solution("tandemtandem"));
+    }
+
+    static boolean solution(String inputString) {
+        return inputString.substring(0, inputString.length() / 2).equals(inputString.substring(inputString.length() / 2));
+    }
+}
