d2e80551cb57d4812dd5a7d46b6cad63caaf6bde
halil-akgun <halilakgun0@gmail.com>
2023-12-27 20:15:53 +0300

    CS102_CrossingSum

---
+++ src/arcade/thecore/CS102_CrossingSum.java
@@ -0,0 +1,30 @@
+package arcade.thecore;
+
+import java.util.Arrays;
+
+/*
+Given a rectangular matrix and integers a and b, consider the union of the ath row and
+the bth (both 0-based) column of the matrix (i.e. all cells that belong either to
+the ath row or to the bth column, or to both). Return sum of all elements of that union.
+
+Example
+For
+matrix = [[1, 1, 1, 1],
+          [2, 2, 2, 2],
+          [3, 3, 3, 3]]
+a = 1, and b = 3, the output should be
+solution(matrix, a, b) = 12.
+
+Here (2 + 2 + 2 + 2) + (1 + 3) = 12.
+ */
+public class CS102_CrossingSum {
+    public static void main(String[] args) {
+        System.out.println(solution(new int[][]{{1, 1, 1, 1}, {2, 2, 2, 2}, {3, 3, 3, 3}}, 1, 3));
+    }
+
+    static int solution(int[][] matrix, int a, int b) {
+        return Arrays.stream(matrix[a]).sum()
+                + Arrays.stream(matrix).mapToInt(t -> t[b]).sum()
+                - matrix[a][b];
+    }
+}
